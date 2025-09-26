7920720677032ba8dd863b32b2cacacaf9ccab37
mcqmyaxh <2290397382@qq.com>
2025-09-26 10:30:15 +0800

    --构建管理员底层架构

---
+++ src/main/java/com/example/yuanqu/Customer/service/ManagementAdminService.java
@@ -0,0 +1,25 @@
+package com.example.yuanqu.Customer.service;
+
+import com.mybatisflex.core.service.IService;
+import com.example.yuanqu.Customer.entity.ManagementAdmin;
+
+import java.util.List;
+
+/**
+ * 管理员表 服务层。
+ *
+ * @author Admin
+ * @since 2025-09-26
+ */
+public interface ManagementAdminService extends IService<ManagementAdmin> {
+    Boolean addAdmin(ManagementAdmin managementAdmin);
+
+    Boolean deleteAdmin(Long adminId);
+
+    Boolean updateAdmin(ManagementAdmin managementAdmin);
+
+    ManagementAdmin getAdmin(ManagementAdmin managementAdmin);
+
+    List<ManagementAdmin> listAdmins(ManagementAdmin managementAdmin);
+
+}
